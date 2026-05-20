0836bb0567bf11c6ce020144cb8c1d5061eb7d91
telarolii <dev.telaroli@gmail.com>
2026-05-20 20:59:30 -0300

    fix: configure dotenv  environment variables

--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,8 @@
 import app from "./app.js";
 import dotenv from "dotenv";
+dotenv.config();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 4000;
 
 const startServer = async () => {
   // It will wait for the database to load before starting the server
