ac76b74b583242df8ad14053efe1b1387ef7346e
telarolii <dev.telaroli@gmail.com>
2026-05-21 17:09:31 -0300

    chore: create frontend project structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 function App() {
   return (
-    <>
-      <div className="min-h-screen bg-zinc-950 text-white flex items-center justify-center">
-        <h1 className="text-5xl font-bold">FinBoard</h1>
-      </div>
-    </>
+    <div className="min-h-screen bg-zinc-950 text-white flex flex-col items-center justify-center gap-6">
+      <h1 className="text-5xl font-bold">FinBoard</h1>
+
+      <button className="px-4 py-2 bg-amber-400 text-black rounded-lg font-medium hover:bg-amber-300 transition">
+        Teste
+      </button>
+    </div>
   );
 }
 
