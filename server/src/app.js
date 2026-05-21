2ed3f8737873829270870808e301e78c460efd1a
telarolii <dev.telaroli@gmail.com>
2026-05-20 21:03:16 -0300

    feat: create health check endpoint

--- server/src/app.js
+++ server/src/app.js
@@ -6,4 +6,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    success: true,
+    status: "ok",
+    body: "server is running",
+  });
+});
+
 export default app;
